faddf8ac0cf859c4111523c349de6592cff938ed
Anna Cai <annacai44@gmail.com>
2025-01-30 16:23:10 -0600

    add find me an expert text

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import { Container, TextField, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, TextField, Button, Typography } from '@mui/material';
 import "./Homepage.css";
 
 function Homepage() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
+      <div className="input-area">
+        <Typography variant="h2">
+          Find me an expert on...
+        </Typography>
         <div id="issue-textfield">
           <TextField
             fullWidth
-            placeholder="Enter in your issue."
+            placeholder="Enter in a topic. e.g. Marbury vs. Madison decision."
             multiline
-            rows={10}
-            maxRows={10}
+            rows={4}
+            onChange={handleChange}
           />
         </div>
         <Button id="legal-expert-button" variant="contained">Find a legal expert!</Button>
+      </div>
     </Container>
   )
 }
